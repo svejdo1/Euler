4c85ea9b7f957090a0999f03895bcf8fe5963f65
Ondrej Svejdar <svejdo1@gmail.com>
2015-11-27 00:29:38 +0100

    problem 77

---
+++ Problem077/Task.cs
@@ -0,0 +1,45 @@
+﻿using Barbar.SymbolicMath.Utilities;
+using System;
+
+namespace Barbar.Euler.Problem077
+{
+    public class Task
+    {
+        public string Run()
+        {
+            int target = 2;
+            int[] primes = new int[1000];
+            int j = 0;
+            for (var i = 2; i < int.MaxValue; i++)
+            {
+                if (MathUtility.IsPrime(i))
+                {
+                    primes[j++] = i;
+                    if (j == primes.Length)
+                        break;
+                }
+            }
+
+
+            while (true)
+            {
+                int[] ways = new int[target + 1];
+                ways[0] = 1;
+
+                for (int i = 0; i < primes.Length; i++)
+                {
+                    for (j = primes[i]; j <= target; j++)
+                    {
+                        ways[j] += ways[j - primes[i]];
+                    }
+                }
+
+                if (ways[target] > 5000)
+                {
+                    return Convert.ToString(target);
+                }
+                target++;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barbar.Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Problem077.Task().Run();
+        }
+    }
+}
