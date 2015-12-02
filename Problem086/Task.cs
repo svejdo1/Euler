ba6d0c21789751929f14bcfdfcb86ee8e8cdfbde
svejdo1 <svejdo1@gmail.com>
2015-12-02 22:41:03 +0100

    100

---
+++ Problem086/Task.cs
@@ -0,0 +1,124 @@
+﻿using Barbar.SymbolicMath.Utilities;
+using System.Collections.Generic;
+
+namespace Barbar.Euler.Problem086
+{
+    public class Task : ITask
+    {
+
+        struct Cuboid
+        {
+            private short a, b, c;
+
+            public Cuboid(short _a, short _b, short _c)
+            {
+                if (_a > _c)
+                    swap(ref _a, ref _c);
+
+                if (_a > _b)
+                    swap(ref _a, ref _b);
+
+                if (_b > _c)
+                    swap(ref _b, ref _c);
+
+                a = _a;
+                b = _b;
+                c = _c;
+            }
+
+            private static void swap(ref short _a, ref short _b)
+            {
+                var xchg = _a;
+                _a = _b;
+                _b = xchg;
+            }
+
+            public override bool Equals(object obj)
+            {
+                var cuboid = (Cuboid)obj;
+                return a == cuboid.a && b == cuboid.b && c == cuboid.c;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("{0},{1},{2}", a, b, c);
+            }
+
+            public override int GetHashCode()
+            {
+                return ((((int)a) << 16) | ((int)b)) ^ ((int)c);
+            }
+        }
+
+        static long Triangles(int limit)
+        {
+            long result = 0;
+            HashSet<Cuboid> uniques = new HashSet<Cuboid>();
+            var maxM = limit;
+            for (var m = 2L; m < maxM; m++)
+            {
+                long nStart = (m & 1) == 1 ? 2L : 1L;
+                for (var n = nStart; n < m; n += 2)
+                {
+                    if (MathUtility.Gcd(n, m) == 1)
+                    {
+                        long c0 = m * m + n * n;
+                        long b0 = m * m - n * n;
+                        long a0 = 2 * m * n;
+                        if (a0 < b0)
+                        {
+                            var xchg = a0;
+                            a0 = b0;
+                            b0 = xchg;
+                        }
+                        if (b0 <= limit && a0 <= 2 * limit)
+                        {
+                            result += HandleTriangle(a0, b0, limit, uniques);
+                        }
+                        long a = a0, b = b0, c = c0;
+                        while (b <= limit && a <= 2 * limit)
+                        {
+                            result += HandleTriangle(a, b, limit, uniques);
+                            a += a0;
+                            b += b0;
+                            c += c0;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static long HandleTriangle(long a, long b, int limit, HashSet<Cuboid> uniques)
+        {
+            int result = 0;
+            for (int j = 1; j < a; j++)
+            {
+                if (j <= limit && a - j <= limit)
+                {
+                    int ta = j;
+                    int tb = (int)(a - j);
+                    int tc = (int)b;
+                    if (tb <= ta && tc <= ta )
+                    {
+                        var cuboid = new Cuboid((short)j, (short)(a - j), (short)b);
+                        if (!uniques.Contains(cuboid))
+                        {
+                            result++;
+                            uniques.Add(cuboid);
+                        }
+                    }
+                    else
+                    {
+                        //throw new Exception();
+                    }
+                }
+            }
+            return result;
+        }
+        public string Run()
+        {
+            return string.Empty;
+        }
+    }
+}
--- Problem100/Task.cs
+++ Problem100/Task.cs
@@ -1,38 +1,53 @@
-﻿using System.Numerics;
+﻿using Barbar.SymbolicMath.Utilities;
+using System.Numerics;
 
 namespace Barbar.Euler.Problem100
 {
+    /// <summary>
+    /// We know that \frac{x}{z}\frac{x-1}{z-1}=\frac{1}{2}
+    /// which leads to 2x^2-2x-z^2+z=0
+    /// x can be than expressed as
+    /// x=\frac{1}{2} + \frac{\sqrt{2z^2-2z+1}}{2}
+    /// if we substitue T^2=2z^2-2z+1 (where T is integer)
+    /// we can express z as z=\frac{1}{2}+\frac{\sqrt{2T^2-1}}{2}
+    /// and we substitue again 2T^2-1=U^2 (where U is integer)
+    /// we get U^2-2T^2=-1 (negative Pell's equation)
+    /// funtamental solution is for this equation is (1,1) and
+    /// recurrent pattern is 
+    /// x_{n+1}=3x_n+4y_n 
+    /// y_{n+1}=2x_n+3y_n
+    /// </summary>
     public class Task : ITask
     {
 
-        public static BigInteger Sqrt(BigInteger n)
-        {
-            int bitLength = (int)BigInteger.Log(n, 2);
-            var root = BigInteger.One << (bitLength >> 1);
-
-            while (n != root * root)
-            {
-                root = (root + (n / root)) >> 1;
-            }
-            return root;
-        }
-
-
         public string Run()
         {
-            var ps = new BigInteger(2828427124744);
-            var z = BigInteger.Pow(new BigInteger(10), 12);
-            while(true)
+            var x = BigInteger.One;
+            var y = BigInteger.One;
+            var limit = new BigInteger(1000000000000);
+            while (true)
             {
-                var t = 8 * z * (z - 1) + 4;
-                var sqrt = Sqrt(t);
-                if (sqrt * sqrt == t)
+                var newX = 3 * x + 4 * y;
+                y = 2 * x + 3 * y;
+                x = newX;
+
+                var z = MathUtility.Sqrt(2 * y * y - 1);
+                if (!z.IsEven)
                 {
-                    break;
+                    z = (z + 1) / 2;
+
+                    var t = 2 * z * z - 2 * z + 1;
+                    var st = MathUtility.Sqrt(t);
+                    if (st * st == t && !st.IsEven)
+                    {
+                        var part = (st + 1) / 2;
+                        if (z > limit)
+                        {
+                            return part.ToString();
+                        }
+                    }
                 }
-                z++;
-            }
-            return string.Empty;
+            };
         }
     }
 }
